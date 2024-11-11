de7f8c188d0cdf8421d943d81dd42cebdd94b28b
burakcanbz <burak.canbaz.web@gmail.com>
2024-11-11 19:17:40 +0300

    feat: homePage refactored visually and added some user friendly features

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Navbar, Nav, Container, Badge, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../slices/usersApiSlice";
-import { logout } from '../slices/authSlice';
+import { logout } from "../slices/authSlice";
 import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/b.png";
 
@@ -18,33 +18,39 @@ const Header = () => {
   const [logoutApiCall] = useLogoutMutation();
 
   const logoutHandler = async () => {
-    try{
-      await logoutApiCall().unwrap(); // without unwrap the logoutApiCall returns redux action object, with it it returns promise-like object 
+    try {
+      await logoutApiCall().unwrap(); // without unwrap the logoutApiCall returns redux action object, with it it returns promise-like object
       dispatch(logout());
-      navigate('/login');
-    }
-    catch(err){
+      navigate("/login");
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="secondary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>
+            <Navbar.Brand className="d-flex align-items-center justify-content-center">
               <img
                 src={logo}
                 alt="Logo"
-                style={{ maxHeight: 50, borderRadius: "50%", marginRight: 10 }}
+                style={{
+                  maxHeight: 50,
+                  borderRadius: "50%",
+                  marginRight: 10,
+                }}
               ></img>
-              BcShop
+              <h2 style={{ marginTop: 6, marginLeft: 10 }}>BCS</h2>
             </Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Nav className="ms-auto">
+              <LinkContainer to="/">
+                <Nav.Link >Products</Nav.Link>
+              </LinkContainer>
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <FaShoppingCart /> Cart{" "}
@@ -59,10 +65,12 @@ const Header = () => {
               </LinkContainer>
               {userInfo ? (
                 <NavDropdown title={userInfo.name} id="username">
-                  <LinkContainer to='/profile'>
-                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
                 </NavDropdown>
               ) : (
                 <LinkContainer to="/login">
--- client/src/components/PaymentForm.jsx
+++ client/src/components/PaymentForm.jsx
@@ -2,59 +2,87 @@ import { PaymentElement } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import { useStripe, useElements } from "@stripe/react-stripe-js";
 import { useSelector, useDispatch } from "react-redux";
-import { setPaidAmount } from "../slices/orderSlice";
-import { usePaySelectedOrderMutation, useGetOrderDetailsQuery } from "../slices/ordersApiSlice";
+import { setPaidAmount, clearOrder } from "../slices/orderSlice";
+import {
+  usePaySelectedOrderMutation,
+  useGetOrderDetailsQuery,
+} from "../slices/ordersApiSlice";
 import { Button } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export default function PaymentForm() {
-
   const { id: orderId } = useParams();
+  const navigate = useNavigate();
+
   const stripe = useStripe();
   const elements = useElements();
-
-  const { orders } = useSelector(state => state.order);
-  const paidAmount = useSelector((state) => state.order.orders[0]?.paidAmount);
-
-  const [ paySelectedOrder ] = usePaySelectedOrderMutation();
-  const { data: order, refetch } = useGetOrderDetailsQuery(orderId);
-
   const dispatch = useDispatch();
 
+  const [paid, setPaid] = useState(false);
   const [message, setMessage] = useState(null);
   const [isProcessing, setIsProcessing] = useState(false);
 
+  const totalPrice = useSelector(
+    (state) =>
+      state.order.orders.find((item) => item._id === orderId)?.totalPrice
+  );
+  const paidAmount = useSelector(
+    (state) =>
+      state.order.orders.find((item) => item._id === orderId)?.paidAmount
+  );
+
+  const [paySelectedOrder] = usePaySelectedOrderMutation();
+  const { data: order, refetch } = useGetOrderDetailsQuery(orderId);
+
   const handleSubmit = async (e) => {
-    // demonstrating payment succeed.
     e.preventDefault();
-    console.log("paid amount => ", paidAmount)
-    console.log(orders[0].totalPrice)
-    dispatch(setPaidAmount({paidAmount: orders[0].totalPrice, orderId}))
+    if (isProcessing) return;
+
+    setIsProcessing(true);
+
+    try {
+      await dispatch(setPaidAmount({ paidAmount: totalPrice, orderId }));
+      await dispatch(clearOrder());
+    } catch (error) {
+      toast.error("Failed to set paid amount.");
+    } finally {
+      setIsProcessing(false);
+    }
   };
 
   useEffect(() => {
-    console.log('in effect ? ')
-    if(paidAmount && paidAmount === orders[0].totalPrice){
-      paySelectedOrder({ amount: orders[0].totalPrice, orderId })
-      .then(_ => {
-        toast.success('Payment succeed')
-        refetch(); 
-      })
-      .catch(error => {
-        toast.error('Payment failed', error)
-      });
+    if (paidAmount && paidAmount === totalPrice) {
+      paySelectedOrder({ amount: totalPrice, orderId })
+        .then((resp) => {
+          if (resp?.data?.updatedOrder?.isPaid) {
+            setPaid(true);
+            toast.success("Payment succeeded");
+            refetch();
+            setTimeout(() => {
+              navigate('/');
+            }, 3000)
+          } else {
+            throw new Error("Payment not marked as paid");
+          }
+        })
+        .catch((error) => {
+          toast.error(`Payment failed: ${error.message}`);
+        });
     }
-  }, [paidAmount, orders[0].totalPrice, paySelectedOrder, refetch])
-
-  
+  }, [paidAmount, totalPrice, paySelectedOrder, refetch]);
 
   return (
     <form id="payment-form" onSubmit={handleSubmit}>
       <PaymentElement id="payment-element" />
-      <Button className='my-2' disabled={isProcessing || !stripe || !elements} id="submit" type="submit">
+      <Button
+        className="my-2"
+        disabled={isProcessing || !stripe || !elements || paid}
+        id="submit"
+        type="submit"
+      >
         <span id="button-text">
-          {isProcessing ? "Processing ... " : "Pay now"}
+          {isProcessing ? "Processing ..." : "Pay now"}
         </span>
       </Button>
       {message && <div id="payment-message">{message}</div>}
