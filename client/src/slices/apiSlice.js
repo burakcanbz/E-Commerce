f07c534a1c72ea043ee2963a00d28f7f82b9dbca
burakcanbz <burak.canbaz.web@gmail.com>
2025-10-08 16:55:49 +0300

    Dockerfile added

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react' ;
-import { logout } from '../slices/authSlice';
+import { logout } from './authSlice';
 import { BASE_URL } from '../constants/constants';
 
 
